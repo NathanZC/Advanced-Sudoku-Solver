5b49065d6ffbf43ed26197798aa169af5334568f
Nathan <58007916+NathanZC@users.noreply.github.com>
2024-02-22 07:47:33 -0800

    inital commit

---
+++ cspmodel.py
@@ -0,0 +1,304 @@
+############################################################
+## CSC 384, Intro to AI, University of Toronto.
+## Assignment 3 Starter Code
+## v1.1
+## Changes:
+##   v1.1: updated the comments in kropki_model.
+##         the second return value should be a 2d list of variables.
+############################################################
+import math
+
+from board import *
+from cspbase import *
+
+
+def kropki_model(board):
+    """
+    Create a CSP for a Kropki Sudoku Puzzle given a board of dimension.
+
+    If a variable has an initial value, its domain should only contain the initial value.
+    Otherwise, the variable's domain should contain all possible values (1 to dimension).
+
+    We will encode all the constraints as binary constraints.
+    Each constraint is represented by a list of tuples, representing the values that
+    satisfy this constraint. (This is the table representation taught in lecture.)
+
+    Remember that a Kropki sudoku has the following constraints.
+    - Row constraint: every two cells in a row must have different values.
+    - Column constraint: every two cells in a column must have different values.
+    - Cage constraint: every two cells in a 2x3 cage (for 6x6 puzzle) 
+            or 3x3 cage (for 9x9 puzzle) must have different values.
+    - Black dot constraints: one value is twice the other value.
+    - White dot constraints: the two values are consecutive (differ by 1).
+
+    Make sure that you return a 2D list of variables separately. 
+    Once the CSP is solved, we will use this list of variables to populate the solved board.
+    Take a look at csprun.py for the expected format of this 2D list.
+
+    :returns: A CSP object and a list of variables.
+    :rtype: CSP, List[List[Variable]]
+
+    """
+    dim = board.dimension
+    csp = CSP("KropkiSudoku")
+    variables = create_variables(dim)
+
+    for row in range(dim):
+        for col in range(dim):
+            var = variables[row][col]
+            if board.cells[row][col] != 0:  # If the cell has a pre-assigned value
+                var.add_domain_values([board.cells[row][col]])
+            else:
+                var.add_domain_values(create_initial_domain(dim))
+
+    for row in variables:
+        for var in row:
+            csp.add_var(var)
+    # Creating different types of constraints
+    diff_tuples = satisfying_tuples_difference_constraints(dim)
+    white_dot_tuples = satisfying_tuples_white_dots(dim)
+    black_dot_tuples = satisfying_tuples_black_dots(dim)
+
+    # Adding Row and Column Constraints
+    row_col_constraints = create_row_and_col_constraints(dim, diff_tuples, variables)
+    for constraint in row_col_constraints:
+        csp.add_constraint(constraint)
+
+    # Adding Cage Constraints
+    cage_constraints = create_cage_constraints(dim, diff_tuples, variables)
+    for constraint in cage_constraints:
+        csp.add_constraint(constraint)
+
+    dot_constraints = create_dot_constraints(dim, board.dots, white_dot_tuples, black_dot_tuples, variables)
+    for constraint in dot_constraints:
+        csp.add_constraint(constraint)
+
+    return csp, variables
+
+
+def create_initial_domain(dim):
+    """
+    Return a list of values for the initial domain of any unassigned variable.
+    [1, 2, ..., dimension]
+
+    :param dim: board dimension
+    :type dim: int
+
+    :returns: A list of values for the initial domain of any unassigned variable.
+    :rtype: List[int]
+    """
+
+    return list(range(1, dim + 1))
+
+
+def create_variables(dim):
+    """
+    Return a list of variables for the board.
+
+    We recommend that your name each variable Var(row, col).
+
+    :param dim: Size of the board
+    :type dim: int
+
+    :returns: A list of variables, one for each cell on the board
+    :rtype: List[Variables]
+    """
+    variables = []
+    for row in range(dim):
+        row_variables = []
+        for col in range(dim):
+            name = f"Var({row},{col})"
+            var = Variable(name)
+            row_variables.append(var)
+        variables.append(row_variables)
+    return variables
+
+
+def satisfying_tuples_difference_constraints(dim):
+    """
+    Return a list of satifying tuples for binary difference constraints.
+
+    :param dim: Size of the board
+    :type dim: int
+
+    :returns: A list of satifying tuples
+    :rtype: List[(int,int)]
+    """
+    sat_tuples = []
+    for i in range(1, dim + 1):
+        for j in range(1, dim + 1):
+            if i != j:
+                sat_tuples.append((i, j))
+    return sat_tuples
+
+
+def satisfying_tuples_white_dots(dim):
+    """
+    Return a list of satifying tuples for white dot constraints.
+
+    :param dim: Size of the board
+    :type dim: int
+
+    :returns: A list of satifying tuples
+    :rtype: List[(int,int)]
+    """
+    sat_tuples = []
+    for i in range(1, dim):
+        sat_tuples.append((i, i + 1))
+        sat_tuples.append((i + 1, i))
+    return sat_tuples
+
+
+def satisfying_tuples_black_dots(dim):
+    """
+    Return a list of satifying tuples for black dot constraints.
+
+    :param dim: Size of the board
+    :type dim: int
+
+    :returns: A list of satifying tuples
+    :rtype: List[(int,int)]
+    """
+    sat_tuples = []
+    for i in range(1, dim // 2 + 1):
+        if 2 * i <= dim:
+            sat_tuples.append((i, 2 * i))
+            sat_tuples.append((i * 2, i))
+    return sat_tuples
+
+
+def create_row_and_col_constraints(dim, sat_tuples, variables):
+    """
+    Create and return a list of binary all-different row/column constraints.
+
+    :param dim: Size of the board
+    :type dim: int
+
+    :param sat_tuples: A list of domain value pairs (value1, value2) such that 
+        the two values in each tuple are different.
+    :type sat_tuples: List[(int, int)]
+
+    :param variables: A list of all the variables in the CSP
+    :type variables: List[Variable]
+        
+    :returns: A list of binary all-different constraints
+    :rtype: List[Constraint]
+    """
+    constraints = []
+
+    # Create row constraints
+    for row in range(dim):
+        for i in range(dim):
+            for j in range(i + 1, dim):
+                constraint = Constraint(f"Row{row}Var{i}Var{j}", [variables[row][i], variables[row][j]])
+                constraint.add_satisfying_tuples(sat_tuples)
+                constraints.append(constraint)
+
+    # Create column constraints
+    for col in range(dim):
+        for i in range(dim):
+            for j in range(i + 1, dim):
+                constraint = Constraint(f"Col{col}Var{i}Var{j}", [variables[i][col], variables[j][col]])
+                constraint.add_satisfying_tuples(sat_tuples)
+                constraints.append(constraint)
+
+    return constraints
+
+
+def create_cage_constraints(dim, sat_tuples, variables):
+    """
+    Create and return a list of binary all-different constraints for all cages.
+
+    :param dim: Size of the board
+    :type dim: int
+
+    :param sat_tuples: A list of domain value pairs (value1, value2) such that 
+        the two values in each tuple are different.
+    :type sat_tuples: List[(int, int)]
+
+    :param variables: A list of all the variables in the CSP
+    :type variables: List[Variable]
+        
+    :returns: A list of binary all-different constraints
+    :rtype: List[Constraint]
+    """
+    constraints = []
+    row_cage_size = 3  # Rows in each cage
+    col_cage_size = 2 if dim == 6 else 3  # Columns in each cage
+
+    for block_row in range(0, dim, row_cage_size):
+        for block_col in range(0, dim, col_cage_size):
+            cage_vars = [variables[block_row + i][block_col + j]
+                         for i in range(row_cage_size)
+                         for j in range(col_cage_size)]
+            for i in range(len(cage_vars)):
+                for j in range(i + 1, len(cage_vars)):
+                    constraint = Constraint(f"Var{i}Var{j}", [cage_vars[i], cage_vars[j]])
+                    constraint.add_satisfying_tuples(sat_tuples)
+                    constraints.append(constraint)
+
+    return constraints
+
+
+def create_dot_constraints(dim, dots, white_tuples, black_tuples, variables):
+    """
+    Create and return a list of binary constraints, one for each dot.
+
+    :param dim: Size of the board
+    :type dim: int
+    
+    :param dots: A list of dots, each dot is a Dot object.
+    :type dots: List[Dot]
+
+    :param white_tuples: A list of domain value pairs (value1, value2) such that 
+        the two values in each tuple satisfy the white dot constraint.
+    :type white_tuples: List[(int, int)]
+    
+    :param black_tuples: A list of domain value pairs (value1, value2) such that 
+        the two values in each tuple satisfy the black dot constraint.
+    :type black_tuples: List[(int, int)]
+
+    :param variables: A list of all the variables in the CSP
+    :type variables: List[Variable]
+        
+    :returns: A list of binary dot constraints
+    :rtype: List[Constraint]
+    """
+    constraints = []
+
+    for dot in dots:
+        # Determine the variables (cells) adjacent to the dot
+        var1 = variables[dot.cell_row][dot.cell_col]
+        var2 = variables[dot.cell2_row][dot.cell2_col]
+
+        # Choose the correct tuples based on the dot's color
+        sat_tuples = black_tuples if dot.color == CHAR_BLACK else white_tuples
+
+        # Create a constraint between the two variables
+        constraint_name = f"DotConstraint_{dot.cell_row}_{dot.cell_col}_{dot.cell2_row}_{dot.cell2_col}"
+        constraint = Constraint(constraint_name, [var1, var2])
+        constraint.add_satisfying_tuples(sat_tuples)
+        constraints.append(constraint)
+
+    return constraints
+#
+# dim = 6
+# csp = CSP("KropkiSudoku")
+# variables = create_variables(dim)
+# for row in variables:
+#     for var in row:
+#         csp.add_var(var)
+#
+# diff_tuples = satisfying_tuples_difference_constraints(dim)
+# white_dot_tuples = satisfying_tuples_white_dots(dim)
+# black_dot_tuples = satisfying_tuples_black_dots(dim)
+#
+# # Adding Row and Column Constraints
+# row_col_constraints = create_row_and_col_constraints(dim, diff_tuples, variables)
+# for constraint in row_col_constraints:
+#     csp.add_constraint(constraint)
+#
+# # Adding Cage Constraints
+# cage_constraints = create_cage_constraints(dim, diff_tuples, variables)
+# for constraint in cage_constraints:
+#     csp.add_constraint(constraint)
---
+++ propagators.py
@@ -0,0 +1,217 @@
+
+
+def prop_FC(csp, last_assigned_var=None):
+    """
+    This is a propagator to perform forward checking. 
+
+    First, collect all the relevant constraints.
+    If the last assigned variable is None, then no variable has been assigned 
+    and we are performing propagation before search starts.
+    In this case, we will check all the constraints.
+    Otherwise, we will only check constraints involving the last assigned variable.
+
+    Among all the relevant constraints, focus on the constraints with one unassigned variable. 
+    Consider every value in the unassigned variable's domain, if the value violates 
+    any constraint, prune the value. 
+
+    :param csp: The CSP problem
+    :type csp: CSP
+        
+    :param last_assigned_var: The last variable assigned before propagation.
+        None if no variable has been assigned yet (that is, we are performing 
+        propagation before search starts).
+    :type last_assigned_var: Variable
+
+    :returns: The boolean indicates whether forward checking is successful.
+        The boolean is False if at least one domain becomes empty after forward checking.
+        The boolean is True otherwise.
+        Also returns a list of variable and value pairs pruned. 
+    :rtype: boolean, List[(Variable, Value)]
+    """
+
+    prunings = []
+
+    # Determine relevant constraints
+    if last_assigned_var is None:
+        relevant_constraints = csp.get_all_cons()
+    else:
+        relevant_constraints = csp.get_cons_with_var(last_assigned_var)
+
+    # Iterate over relevant constraints
+    for constraint in relevant_constraints:
+        if constraint.get_num_unassigned_vars() == 1:
+            unassigned_var = constraint.get_unassigned_vars()[0]
+            for value in unassigned_var.cur_domain():
+                if not constraint.check(
+                        [value] + [var.get_assigned_value() for var in constraint.get_scope() if var.is_assigned()]):
+                    prunings.append((unassigned_var, value))
+                    unassigned_var.prune_value(value)
+
+    # Check if any domain is empty
+    for var in csp.get_all_vars():
+        if var.cur_domain_size() == 0:
+            return False, prunings
+
+    return True, prunings
+
+
+def prop_AC3(csp, last_assigned_var=None):
+    """
+    This is a propagator to perform the AC-3 algorithm.
+
+    Keep track of all the constraints in a queue (list). 
+    If the last_assigned_var is not None, then we only need to 
+    consider constraints that involve the last assigned variable.
+
+    For each constraint, consider every variable in the constraint and 
+    every value in the variable's domain.
+    For each variable and value pair, prune it if it is not part of 
+    a satisfying assignment for the constraint. 
+    Finally, if we have pruned any value for a variable,
+    add other constraints involving the variable back into the queue.
+
+    :param csp: The CSP problem
+    :type csp: CSP
+        
+    :param last_assigned_var: The last variable assigned before propagation.
+        None if no variable has been assigned yet (that is, we are performing 
+        propagation before search starts).
+    :type last_assigned_var: Variable
+
+    :returns: a boolean indicating if the current assignment satisifes 
+        all the constraints and a list of variable and value pairs pruned. 
+    :rtype: boolean, List[(Variable, Value)]
+    """
+    if last_assigned_var is not None:
+        # Initialize the queue with constraints involving the last assigned variable
+        queue = csp.get_cons_with_var(last_assigned_var)
+    else:
+        # Initialize the queue with all constraints
+        queue = list(csp.get_all_cons())
+
+    prunings = []
+
+    while queue:
+        constraint = queue.pop(0)
+        for var in constraint.get_scope():
+            for value in var.cur_domain():
+                if not is_value_consistent(constraint, var, value):
+                    prunings.append((var, value))
+                    var.prune_value(value)
+                    if var.cur_domain_size() == 0:
+                        # If domain is empty, return failure
+                        return False, prunings
+                    else:
+                        # Add related constraints back to the queue
+                        for related_constraint in csp.get_cons_with_var(var):
+                            if related_constraint != constraint:
+                                queue.append(related_constraint)
+    return True, prunings
+
+
+def is_value_consistent(constraint, var, value):
+    """
+    Check if a value is consistent with the given constraint.
+    A value is consistent if it can be part of a satisfying assignment for the constraint.
+
+    :param constraint: The constraint to check
+    :type constraint: Constraint
+    :param var: The variable to check the value for
+    :type var: Variable
+    :param value: The value to check
+    :type value: int
+    :returns: True if the value is consistent, False otherwise
+    :rtype: bool
+    """
+    for other_vars in constraint.get_scope():
+        if other_vars != var:
+            # Check all combinations of other variable values to see if
+            # there exists at least one that satisfies the constraint with the given value.
+            for other_val in other_vars.cur_domain():
+                if constraint.check([value if v == var else other_val for v in constraint.get_scope()]):
+                    return True
+    return False
+
+
+
+def ord_mrv(csp):
+    """
+    Implement the Minimum Remaining Values (MRV) heuristic.
+    Choose the next variable to assign based on MRV.
+
+    If there is a tie, we will choose the first variable. 
+
+    :param csp: A CSP problem
+    :type csp: CSP
+
+    :returns: the next variable to assign based on MRV
+
+    """
+
+    unasgn_vars = csp.get_all_unasgn_vars()
+
+    # If there are no unassigned variables, return None
+    if not unasgn_vars:
+        return None
+
+    # Find the variable with the minimum remaining values in its domain
+    min_domain_size = min(var.cur_domain_size() for var in unasgn_vars)
+    for var in unasgn_vars:
+        if var.cur_domain_size() == min_domain_size:
+            return var
+
+    # In case no variable is found (which shouldn't happen), return None
+    return None
+
+
+###############################################################################
+# Do not modify the prop_BT function below
+###############################################################################
+
+
+def prop_BT(csp, last_assigned_var=None):
+    """
+    This is a basic propagator for plain backtracking search.
+
+    Check if the current assignment satisfies all the constraints.
+    Note that we only need to check all the fully instantiated constraints
+    that contain the last assigned variable.
+
+    :param csp: The CSP problem
+    :type csp: CSP
+
+    :param last_assigned_var: The last variable assigned before propagation.
+        None if no variable has been assigned yet (that is, we are performing
+        propagation before search starts).
+    :type last_assigned_var: Variable
+
+    :returns: a boolean indicating if the current assignment satisifes all the constraints
+        and a list of variable and value pairs pruned.
+    :rtype: boolean, List[(Variable, Value)]
+
+    """
+
+    # If we haven't assigned any variable yet, return true.
+    if not last_assigned_var:
+        return True, []
+
+    # Check all the constraints that contain the last assigned variable.
+    for c in csp.get_cons_with_var(last_assigned_var):
+
+        # All the variables in the constraint have been assigned.
+        if c.get_num_unassigned_vars() == 0:
+
+            # get the variables
+            vars = c.get_scope()
+
+            # get the list of values
+            vals = []
+            for var in vars: #
+                vals.append(var.get_assigned_value())
+
+            # check if the constraint is satisfied
+            if not c.check(vals):
+                print(c.name)
+                return False, []
+
+    return True, []
